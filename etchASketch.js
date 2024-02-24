4b67d1c9bbb1e6fa8f2d14632600775e0a6dcad8
ragunath-br <153410999+ragunath-br@users.noreply.github.com>
2024-02-24 18:40:46 +0530

    FIX FOR DIV CREATION FUNCTION
    Fixes 2 issues
    1. Uses the correct creation method for creating a DocumentFragment
    2. Modifies the row and colum to ensure uniform naming of divs within the container.

--- etchASketch.js
+++ etchASketch.js
@@ -3,11 +3,11 @@ createGrid(16,16);
 
 
 function createGrid(rows,columns){
-    const docFragment = document.createDocumentFragment;
+    const docFragment = document.createDocumentFragment();
     for (let i = 1; i <= rows; i++){
         for (let j = 1;j <= columns; j++){
             let divElement = document.createElement('div');
-            divElement.id = `div_${i}${j}`;
+            divElement.id = `div_${('0' + i).slice(-2)}${('0' + j).slice(-2)}`;
             docFragment.appendChild(divElement);
         }
     }
